c6a4485895be5591b15d7571041b3a6c48478d53
Jenath <jenathkanagesan@gmail.com>
2025-08-08 09:59:17 +0100

    check background image relative url

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,12 +1,12 @@
 import "./section_style/Home.css"
-import home_image from "../assets/jpg/plants.jpg"
+// import home_image from "../assets/jpg/plants.jpg"
 
 const Home = () => {
 
     return (
         <>
             <section class="home">
-                <img src={home_image} width="50%" alt="" />
+                {/* <img src={home_image} alt="" /> */}
             </section>
         </>
     )
